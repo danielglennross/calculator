3d27a040512ca7b8bb845eadc3b06a4e2c38eabd
Daniel Ross <daniel.ross@form3.tech>
2022-06-26 15:31:58 +0100

    feat: init commit

---
+++ main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+type binary struct {
+	left, right interface{}
+	op          string
+}
+
+func main() {
+	printCalculation("1 + 2 * (3 + 4) - 5 * (6 * (7 + 8))")
+}
+
+func printCalculation(input string) {
+	res := calculate(input)
+	fmt.Printf("%v: value is: %v\n", input, res)
+}
+
+func calculate(input string) int {
+	tokenMap, charIndex := map[string][]string{}, -1
+
+	chars := toChars(input)
+	tokenizedChars := tokenizeBrackets(&charIndex, chars, tokenMap, tokenGenerator())
+
+	groups := createBinaryGroups(tokenizedChars, tokenMap)
+	result := buildComputeFrom(
+		computeBinary("*"),
+		computeBinary("/"),
+		computeBinary("+"),
+		computeBinary("-"),
+	)(groups)
+
+	return result
+}
+
+func buildComputeFrom(compute ...func([]*binary) []*binary) func([]*binary) int {
+	return func(ops []*binary) int {
+		acc := compute[0](ops)
+		for _, c := range compute[1:] {
+			acc = c(acc)
+		}
+		// we should be left with only 1 group
+		return evalBinary(acc[0])
+	}
+}
+
+func tokenizeBrackets(charIndex *int, chars []string, tokens map[string][]string, nextToken func() string) []string {
+	var charsWithTokens []string
+	i := 0
+	for i < len(chars) {
+		*charIndex++
+		c := chars[i]
+		if c == ")" {
+			return charsWithTokens
+		}
+		if c == "(" {
+			t := nextToken()
+			tokens[t] = tokenizeBrackets(charIndex, chars[i+1:], tokens, nextToken)
+			charsWithTokens = append(charsWithTokens, t)
+			i = *charIndex + 1
+		} else {
+			charsWithTokens = append(charsWithTokens, c)
+			i++
+		}
+	}
+	return charsWithTokens
+}
+
+func createBinaryGroups(chars []string, tokenMap map[string][]string) []*binary {
+	var ops []*binary
+	for i, c := range chars {
+		if !strings.ContainsAny(c, "*/+-") {
+			continue
+		}
+
+		var left interface{}
+		left, err := strconv.Atoi(chars[i-1])
+		if err != nil {
+			left = createBinaryGroups(tokenMap[chars[i-1]], tokenMap)
+		}
+
+		var right interface{}
+		right, err = strconv.Atoi(chars[i+1])
+		if err != nil {
+			right = createBinaryGroups(tokenMap[chars[i+1]], tokenMap)
+		}
+
+		ops = append(ops, &binary{
+			left:  left,
+			right: right,
+			op:    c,
+		})
+	}
+	return ops
+}
+
+func computeBinary(symbol string) func([]*binary) []*binary {
+	maybeSetPrevBinary := func(l int, ops []*binary, res int) {
+		for l > 0 {
+			if ops[l-1] != nil {
+				leftSide := ops[l-1]
+				leftSide.right = res
+				break
+			}
+			l--
+		}
+	}
+
+	maybeSetNextBinary := func(r int, ops []*binary, res int) {
+		for r+1 < len(ops) {
+			if ops[r+1] != nil {
+				rightSide := ops[r+1]
+				rightSide.left = res
+				break
+			}
+			r++
+		}
+	}
+
+	return func(ops []*binary) []*binary {
+		for i, b := range ops {
+			if isSingleBinaryRemaining(ops) {
+				break
+			}
+			if b.op != symbol {
+				continue
+			}
+
+			res := evalBinary(b)
+			ops[i] = nil
+
+			maybeSetPrevBinary(i, ops, res)
+			maybeSetNextBinary(i, ops, res)
+		}
+
+		return filterNils(ops)
+	}
+}
+
+func evalBinary(b *binary) int {
+	compute := buildComputeFrom(
+		computeBinary("*"),
+		computeBinary("/"),
+		computeBinary("+"),
+		computeBinary("-"),
+	)
+
+	var left int
+	if l, ok := b.left.([]*binary); ok {
+		left = compute(l)
+	} else {
+		left = b.left.(int)
+	}
+
+	var right int
+	if r, ok := b.right.([]*binary); ok {
+		right = compute(r)
+	} else {
+		right = b.right.(int)
+	}
+
+	switch b.op {
+	case "*":
+		return left * right
+	case "/":
+		return left / right
+	case "+":
+		return left + right
+	case "-":
+		return left - right
+	default:
+		return 0
+	}
+}
+
+func toChars(input string) []string {
+	var chars []string
+	for _, s := range input {
+		if strings.Trim(string(s), " ") == "" {
+			continue
+		}
+		chars = append(chars, string(s))
+	}
+	return chars
+}
+
+func filterNils(ops []*binary) []*binary {
+	var newOps []*binary
+	for _, b := range ops {
+		if b != nil {
+			newOps = append(newOps, b)
+		}
+	}
+	return newOps
+}
+
+func isSingleBinaryRemaining(ops []*binary) bool {
+	var i int
+	for _, b := range ops {
+		if b == nil {
+			i++
+		}
+	}
+	return i == len(ops)-1
+}
+
+func tokenGenerator() func() string {
+	tokens, i, r := "abcdefghijklmnopqrstuvwxyz", 0, 0
+	return func() string {
+		defer func() { i++ }()
+		if i%len(tokens) == 0 {
+			i = 0
+			r++
+		}
+		return strings.Repeat(string(tokens[i]), r)
+	}
+}
